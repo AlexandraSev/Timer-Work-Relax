cb4ae4b87393d37e1902d1ccf4dfee23594266dd
Alexandra <AlexandraSevost@icloud.com>
2022-09-12 22:06:59 +0300

    Create timer and implementation via button

--- Timer Work\Relax/Sources/Storyboards/App/ViewController.swift
+++ Timer Work\Relax/Sources/Storyboards/App/ViewController.swift
@@ -9,44 +9,80 @@ import UIKit
 
 class ViewController: UIViewController {
     // MARK: - Outlets
-    private lazy var isStarted = true
+    private lazy var isStarted = false
     private lazy var isWorkTime = true
+    
+    // MARK: - Timer
+    var timer: Timer?
+    var runCountJob = 26
+    var runCountRelax = 11
+    
     
     // MARK: - Label Time
     private lazy var labelTimer: UILabel = {
         let label = UILabel()
-        label.text = "25:00"
+        label.text = "Start"
         label.textAlignment = .center
         label.font = UIFont.systemFont(ofSize: 50, weight: .light)
-        if isWorkTime == true {
-            label.textColor = .systemGreen
-        } else {
-            label.textColor = .red }
+        label.textColor = .black
+        
         return label
     }()
     
-  
+    
     // MARK: - Button Play/Pause
     private lazy var buttonPlayOrPause: UIButton = {
         let button = UIButton()
         button.setImage(UIImage(named: "play"), for: .normal)
-        button.tintColor = .systemGreen
+        
+        
+        button.tintColor = .black
         button.clipsToBounds = false
         button.addTarget(self, action: #selector(ourButtonPressed), for: .touchUpInside)
         return button
     }()
     
-   
+    
     //MARK: - Lifestyle
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
         setupHierarchy()
         setupLayout()
         labelTimer.translatesAutoresizingMaskIntoConstraints = false
         buttonPlayOrPause.translatesAutoresizingMaskIntoConstraints = false
     }
     
+    @objc func fireTimer() {
+        
+        if isStarted == true {
+            if isWorkTime == true {
+                
+                runCountJob -= 1
+                labelTimer.text = "\(runCountJob):00"
+                labelTimer.textColor = .systemGreen
+                buttonPlayOrPause.tintColor = .systemGreen
+                if runCountJob == 0 {
+                    labelTimer.textColor = .black
+                    buttonPlayOrPause.tintColor = .red
+                    isWorkTime = !isWorkTime
+                    runCountJob = 26
+                    
+                }
+            } else {
+                runCountRelax -= 1
+                labelTimer.text = "\(runCountRelax):00"
+                labelTimer.textColor = .red
+                if runCountRelax == 0 {
+                    labelTimer.textColor = .black
+                    buttonPlayOrPause.tintColor = .black
+                    isWorkTime = !isWorkTime
+                    runCountRelax = 11
+                }
+            }
+        }
+    }
     // MARK: - Setup
     private func setupHierarchy() {
         view.addSubview(labelTimer)
@@ -67,16 +103,13 @@ class ViewController: UIViewController {
     }
     
     // MARK: - Actions
-    func getRandomColor() -> UIColor{
-        let red: CGFloat = CGFloat(drand48())
-        let green: CGFloat = CGFloat(drand48())
-        let blue: CGFloat = CGFloat(drand48())
-        
-        return UIColor( red: red, green: green, blue: blue, alpha: 1.0)
-    }
+    
     @objc private func ourButtonPressed() {
-        
-            buttonPlayOrPause.setImage(UIImage(named: "pause"), for: .normal)}
-        
+        isStarted = !isStarted
+        if isStarted == true {
+            buttonPlayOrPause.setImage(UIImage(named: "pause"), for: .normal) } else {  buttonPlayOrPause.setImage(UIImage(named: "play"), for: .normal)
+            }
     }
-
+    
+    
+}
